756cdc847041eb29738218f16b2dfc59c9896284
Soeler <102874847+soeler1337@users.noreply.github.com>
2025-07-28 02:10:45 +0200

    Update token.js

--- api/token.js
+++ api/token.js
@@ -1,4 +1,9 @@
 export default async function handler(req, res) {
+   // CORS Header setzen
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Für Sicherheit: später Domain eintragen
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   const bearer = process.env.TWITCH_BF_BEARER;
   res.status(200).json({ access_token: bearer });
 }
