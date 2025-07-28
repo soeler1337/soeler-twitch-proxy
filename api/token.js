75e8057a5f24cbfa4c8a8e7bdf0098ef28aa7661
Soeler <102874847+soeler1337@users.noreply.github.com>
2025-07-28 02:13:42 +0200

    Update token.js

--- api/token.js
+++ api/token.js
@@ -5,5 +5,6 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   const bearer = process.env.TWITCH_BF_BEARER;
-  res.status(200).json({ access_token: bearer });
+  const clientId = process.env.TWITCH_BF_TWITCH_BF_CLIENT_ID;
+  res.status(200).json({ access_token: bearer, clientId: clientId });
 }
